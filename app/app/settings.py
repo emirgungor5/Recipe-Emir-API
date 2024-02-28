23138ef2bfdc46fad804386c4a5b221730158dfb
emirgungor5 <emirgungor_141@outlook.com>
2024-02-28 13:54:44 +0300

    Last Push

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular',
+
 
 ]
 
@@ -131,3 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'core.User'
+
+REST_FRAMEWORK = {
+    'DEFAUL_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+
+}
